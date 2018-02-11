54f5a6cc657a7ad075ffecafc37c4bf479d7af75
gc5k <chenguobo@gmail.com>
2018-02-11 15:12:24 +0800

    modify EigenGWAS_Friends.R

--- EigenGWAS_Friends/EigenGWAS_Friends.R
+++ EigenGWAS_Friends/EigenGWAS_Friends.R
@@ -1,51 +1,73 @@
-EigenQQPlot <- function(root, pc, pdf="n")
+
+fCheck <- function(fn)
 {
-  eg = read.table(paste0(root, ".", pc, ".egwas"), as.is = T, header = T)
-  if (pdf == "pdf")
+  if (!file.exists(fn))
   {
-    pdf(paste0(root, "_grm.pdf"))
-  }
-
-  gc = qchisq(median(eg$P), 1, lower.tail = F)/qchisq(0.5, 1)
-  thchi = rchisq(which(!is.na(eg$P)), 1)
-  qqplot(main="", thchi, eg$Chi, xlab=expression(paste("Theoretical ", chi[1]^2)), ylab=expression(paste("EigenGWAS ", chi[1]^2)), frame.plot=F, pch=16, cex=0.5) 
-  points(sort(thchi), sort(eg$Chi[!is.na(eg$Chi)]/gc), pch=1, cex=0.7, col="blue")
-  abline(a=0, b=1, col="red", lty=2)
-  legend("topleft", legend = c(paste0("LambdaGC=", format(gc, digits=3, nsmall=2))), bty='n')
-
-  if (pdf == "pdf")
-  {
-    dev.off()
+    print(fn, " does not exist.")
+    return(F)
+  } else {
+    return(T)
   }
 }
 
+EigenQQPlot <- function(root, pc)
+{
+  if(!fCheck(paste0(root, ".", pc, ".egwas")))
+  {
+    return()
+  }
 
-grmStats <- function(root, pdf="n")
+  eg = read.table(paste0(root, ".", pc, ".egwas"), as.is = T, header = T)
+
+  gc = qchisq(median(eg$P), 1, lower.tail = F)/qchisq(0.5, 1)
+  thchi = rchisq(which(!is.na(eg$P)), 1)
+  qqplot(main="", thchi, eg$Chi, xlab=expression(paste("Theoretical ", chi[1]^2)), ylab=expression(paste("EigenGWAS ", chi[1]^2)), frame.plot=F, pch=16, cex=0.5, col="grey")
+  points(sort(thchi), sort(eg$Chi[!is.na(eg$Chi)]/gc), pch=1, cex=0.7)
+  abline(a=0, b=1, col="red", lty=2)
+  legend("topleft", legend = c(paste0("LambdaGC=", format(gc, digits=3, nsmall=2))), bty='n')
+}
+
+grmStats <- function(root)
 {
   grm=grmReader(root)
   grmS=grm[col(grm) > row(grm)]
   ne=-1/mean(grmS)
   me=1/var(grmS)
-  if (pdf == "pdf")
-  {
-    pdf(paste0(root, "_grm.pdf"))
-  }
   hist(grmS, xlab="GRM scores", main ="GRM distribution", breaks=25)
   legend("topright", legend = c(paste0("ne=", format(ne, digits=3, nsmall=2)), paste0("me=", format(me, digits=3, nsmall=2))), bty='n')
-
-  if (pdf == "pdf")
-  {
-    dev.off()
-  }
 }
 
-pcMatPlot <- function(root, pc=2, pdf="n", COL="grey", ma=0.3)
+grmReader <- function(root)
 {
-  Evec=read.table(paste0(root, ".eigenvec"), as.is = T)
-  if(pdf == "pdf")
+  if(!fCheck(paste0(root, ".grm.gz")))
   {
-    pdf(paste0(root, "_pcMat.pdf"))
+    return()
   }
+  grmFile = gzfile(paste0(root, ".grm.gz"))
+  grm = read.table(grmFile)
+
+  idFile = read.table(paste0(root, ".grm.id"), as.is=T)
+
+  mat=matrix(0, nrow(idFile), nrow(idFile))
+  for(i in 1:nrow(idFile))
+  {
+    idx1 = i * (i-1)/2 + 1
+    idx2 = i * (i+1)/2
+    mat[i, 1:i] = grm[idx1:idx2, 4]
+    mat[1:i, i] = grm[idx1:idx2, 4]
+  }
+  return(mat)
+}
+
+pcMatPlot <- function(root, PC=c(1,2), COL="grey", ma=0.3)
+{
+  if(!fCheck(paste0(root, ".eigenvec")))
+  {
+    return()
+  }
+
+  pc=length(PC)
+  Evec=read.table(paste0(root, ".eigenvec"), as.is = T)
   par(mai=c(ma, ma, ma, ma))
   nt=pc*(pc-1)/2
   mat=matrix(nt+pc+1, pc, pc)
@@ -56,7 +78,7 @@ pcMatPlot <- function(root, pc=2, pdf="n", COL="grey", ma=0.3)
   {
     for(j in (i+1):pc)
     {
-      plot(Evec[,i+2],Evec[, j+2], cex=0.5, pch=16, col=COL, xlim=1.2*range(Evec[,i+2]), ylim=1.2*range(Evec[,j+2]), bty="l", axes = F)
+      plot(Evec[,PC[i]+2],Evec[, PC[j]+2], cex=0.5, pch=16, col=COL, xlim=1.2*range(Evec[,PC[i]+2]), ylim=1.2*range(Evec[,PC[j]+2]), bty="l", axes = F)
       abline(h=0, v=0, col="grey70", lty=2)
     }
   }
@@ -64,39 +86,34 @@ pcMatPlot <- function(root, pc=2, pdf="n", COL="grey", ma=0.3)
   {
     if (i== 1)
     {
-      plot(x=NULL, y=NULL, xlim=1.2*range(Evec[,i+2]), ylim=1.2*range(Evec[,i+2]), bty="l")
+      plot(x=NULL, y=NULL, xlim=1.2*range(Evec[,PC[i]+2]), ylim=1.2*range(Evec[,PC[i]+2]), bty="l")
     } else if (i == pc)
     {
-      plot(x=NULL, y=NULL, xlim=1.2*range(Evec[,i+2]), ylim=1.2*range(Evec[,i+2]), bty="l")
+      plot(x=NULL, y=NULL, xlim=1.2*range(Evec[,PC[i]+2]), ylim=1.2*range(Evec[,PC[i]+2]), bty="l")
     } else {
-      plot(x=NULL, y=NULL, xlim=1.2*range(Evec[,i+2]), ylim=1.2*range(Evec[,i+2]), bty="l")
+      plot(x=NULL, y=NULL, xlim=1.2*range(Evec[,PC[i]+2]), ylim=1.2*range(Evec[,PC[i]+2]), bty="l")
     }
-    text(mean(1.2*range(Evec[,i+2])), mean(1.2*range(Evec[,i+2])),labels = paste("EV", i))
-  }
-  if(pdf == "pdf")
-  {
-    dev.off()
+    text(mean(1.2*range(Evec[,PC[i]+2])), mean(1.2*range(Evec[,PC[i]+2])),labels = paste("EV", PC[i]))
   }
 }
 
-pcPlot <- function(root, pdf="n")
+EigenValuePlot <- function(root, PC = 1)
 {
-  Evec=read.table(paste0(root, ".eigenvec"), as.is = T)
-  if(pdf == "pdf")
+  if (!fCheck(paste0(root, ".eigenval")))
   {
-    pdf(paste0(root, "_pc.pdf"))
+    return()
   }
-  plot(Evec[,3], Evec[,4], pch=16, xlab="PC 1", ylab="PC 2", frame.plot = F)
-  if(pdf == "pdf")
-  {
-    dev.off()
-  }
-}
 
-EigenValuePlot <- function(root, PC, pdf="n")
-{
   Evev=read.table(paste0(root, ".eigenval"), as.is = T)
   GC=array(0, dim=PC)
+
+  for(i in 1:PC)
+  {
+    if (!fCheck(paste0(root, ".", i, ".egwas")))
+    {
+      return()
+    }
+  }
 
   for(i in 1:PC)
   {
@@ -106,50 +123,40 @@ EigenValuePlot <- function(root, PC, pdf="n")
 
   egc=matrix(c(Evev[1:PC,1], GC), PC, 2, byrow = F)
   rownames(egc)=seq(1, PC)
-  if(pdf == "pdf")
-  {
-    pdf(paste0(FN, "_EV.pdf"))
-  }
   barplot(t(egc), beside = T, border = F)
   legend("topright", legend = c("Eigenvalue", expression(paste(lambda[gc]))), pch=15, col=c("black", "grey"), bty='n')
-  if(pdf == "pdf")
-  {
-    dev.off()
-  }
 }
 
-DeepEigenValuePlot <- function(root, PC, cutV, pdf="n")
+DeepEigenValuePlot <- function(root, PC, qcut=c(0.25, 0.5, 0.75))
 {
+  if (!fCheck(paste0(root, ".eigenval")))
+  {
+    return()
+  }
+
   Evev=read.table(paste0(root, ".eigenval"), as.is = T)
-  GC=matrix(0, nrow=1, ncol=length(cutV)+1)
+  GC=matrix(0, nrow=1, ncol=length(qcut)+1)
   GC[1]=Evev[PC,1]
   eg = read.table(paste0(root, ".", PC, ".egwas"), as.is = T, header = T)
 
-  for(i in 1:length(cutV))
-  {
-    GC[1,1+i] = qchisq(sort(eg$P, T)[ceiling(cutV[i]*nrow(eg))], 1, lower.tail = F)/qchisq(cutV[i], 1, lower.tail = T)
-  }
-  colnames(GC)=c("EV", cutV)
+  GC[1, 2:(1+length(qcut))] = qchisq(sort(eg$P)[ceiling(qcut[i]*nrow(eg))], 1, lower.tail = T)/qchisq(qcut[i], 1, lower.tail = T)
+  colnames(GC)=c("EV", qcut)
   par(las=2)
-  barplot(GC, ylim=c(0, max(GC)*1.3), beside = T, border = F, col=c("black", rep("grey", length(cutV))))
+  barplot(GC, ylim=c(0, max(GC)*1.3), beside = T, border = F, col=c("black", rep("grey", length(qcut))))
   legend("topright", legend = c("Eigenvalue", expression(paste(lambda[gc]))), pch=15, col=c("black", "grey"), bty='n')
-  if(pdf == "pdf")
-  {
-    dev.off()
-  }
 }
 
-
-EigenGWASPlot <- function(root, pc, pdf='n')
+EigenGWASPlot <- function(root, pc)
 {
+  if(!fCheck(paste0(root, ".", pc, ".egwas")))
+  {
+    return()
+  }
+
   eg=read.table(paste0(root, ".", pc, ".egwas"), as.is = T, header = T)
 
-  if(pdf == "pdf")
-  {
-    pdf(paste0(root, "_E", pc, ".pdf"))
-  }
   layout(matrix(1:3, 3, 1))
-  
+
   eg=eg[,-which(colnames(eg)=="P")]
   colnames(eg)[which(colnames(eg)=="PGC")]="P"
   manhattan(eg, pch=16, cex=0.5)
@@ -157,15 +164,15 @@ EigenGWASPlot <- function(root, pc, pdf='n')
   plot(eg$Chi, eg$Fst, xlab=expression(chi[1]^2), ylab=expression(paste(italic("F")[italic("ST")])), pch=16, cex=0.5, frame.plot = F)
   rsq=cor(eg$Chi, eg$Fst, use="pairwise.complete.obs")^2
   legend("topright", legend = c(paste("Rsq=", format(rsq, digits = 4, nsmall = 3))), bty='n')
-
-  if(pdf == "pdf")
-  {
-    dev.off()
-  }
 }
 
-SWEigenGWASPlot <- function(root, pc, kb=5, pdf='n')
+SWEigenGWASPlot <- function(root, pc, kb=5)
 {
+  if(!fCheck(paste0(root, ".", pc, ".egwas")))
+  {
+    return()
+  }
+
   eg=read.table(paste0(root, ".", pc, ".egwas"), as.is = T, header = T)
   lgc=qchisq(median(eg$P), 1, lower.tail = F)/qchisq(0.5, 1)
   CHR=names(table(eg$CHR))
@@ -174,7 +181,7 @@ SWEigenGWASPlot <- function(root, pc, kb=5, pdf='n')
     egs=eg[which(eg$CHR == CHR[i]),]
     rg=range(egs$BP)
     meg=matrix(0, nrow(egs), 5)
-    
+
     for(j in 1:nrow(meg))
     {
       idx=which((egs$BP >= egs$BP[j] - kb*1000) & (egs$BP <= egs$BP[j] + kb * 1000))
@@ -196,11 +203,6 @@ SWEigenGWASPlot <- function(root, pc, kb=5, pdf='n')
   DMeg=as.data.frame(Meg)
   colnames(DMeg)=c("CHR", "BP", "Fst", "P", "Chi")
 
-  
-  if(pdf == "pdf")
-  {
-    pdf(paste0(root, "_SE", pc, ".pdf"))
-  }
   layout(matrix(1:3, 3, 1))
 
   manhattan(DMeg, cex=0.5, pch=16)
@@ -208,15 +210,20 @@ SWEigenGWASPlot <- function(root, pc, kb=5, pdf='n')
   plot(DMeg$Chi, DMeg$Fst, xlab=expression(chi[1]^2), ylab=expression(paste(italic("F")[italic("ST")])), pch=16, cex=0.5, frame.plot = F)
   rsq=cor(DMeg$Chi, DMeg$Fst, use="pairwise.complete.obs")^2
   legend("topright", legend = c(paste("Rsq=", format(rsq, digits = 4, nsmall = 3))), bty='n')
-  
-  if(pdf == "pdf")
-  {
-    dev.off()
-  }
+
 }
 
-SWPhenoEigenGWASPlot <- function(phe, root, pc, kb=5, pdf='n')
+SWPhenoEigenGWASPlot <- function(phe, root, pc, kb=5)
 {
+  if (!fCheck(paste0(root, ".", pc, ".egwas")))
+  {
+    return()
+  }
+  if (!fCheck(phe))
+  {
+    return()
+  }
+
   eg=read.table(paste0(root, ".", pc, ".egwas"), as.is = T, header = T)
   gw=read.table(phe, as.is = T, header = T)
 
@@ -227,7 +234,7 @@ SWPhenoEigenGWASPlot <- function(phe, root, pc, kb=5, pdf='n')
     egs=eg[which(eg$CHR == CHR[i]),]
     rg=range(egs$BP)
     meg=matrix(0, nrow(egs), 6)
-    
+
     for(j in 1:nrow(meg))
     {
       idx=which((egs$BP >= egs$BP[j] - kb*1000) & (egs$BP <= egs$BP[j] + kb * 1000))
@@ -238,7 +245,7 @@ SWPhenoEigenGWASPlot <- function(phe, root, pc, kb=5, pdf='n')
       meg[j, 5] = mean(egs$Chi[idx])
       meg[j, 6] = pchisq(mean(qchisq(gw$P[idx], 1, lower.tail = F)), 1, lower.tail = F)
     }
-    
+
     if(i == 1)
     {
       Meg = meg
@@ -253,48 +260,19 @@ SWPhenoEigenGWASPlot <- function(phe, root, pc, kb=5, pdf='n')
   colnames(DMegE)=c("CHR", "BP", "Fst", "P", "Chi")
   colnames(DMegG)=c("CHR", "BP", "P")
 
-  if(pdf == "pdf")
-  {
-    pdf(paste0(root, "_SE", pc, ".pdf"))
-  }
   layout(matrix(1:3, 3, 1))
 
   manhattan(DMegE, cex=0.5, pch=16)
   manhattan(DMegG, cex=0.5, pch=16)
 
-#  FstPlot(DMeg, pch=16, cex=0.5)
-#  plot(DMeg$Chi, DMeg$Fst, xlab=expression(chi[1]^2), ylab=expression(paste(italic("F")[italic("ST")])), pch=16, cex=0.5, frame.plot = F)
   plot(-log10(DMegE$P), -log10(DMegG$P), xlab=expression(paste(-log[10](italic(p)), "[EigenGWAS]")), ylab=expression(paste(-log[10](italic(p)), "[GWAS]")), pch=16, cex=0.5, frame.plot = F)
 
   rsq=cor(-log10(DMegE$P), -log10(DMegG$P), use="pairwise.complete.obs")^2
   legend("topright", legend = c(paste("Rsq=", format(rsq, digits = 4, nsmall = 3))), bty='n')
-  
-  if(pdf == "pdf")
-  {
-    dev.off()
-  }
-}
-
-grmReader <- function(root)
-{
-  grmFile = gzfile(paste0(root, ".grm.gz"))
-  grm = read.table(grmFile)
-  
-  idFile = read.table(paste0(root, ".grm.id"), as.is=T)
-  
-  mat=matrix(0, nrow(idFile), nrow(idFile))
-  for(i in 1:nrow(idFile))
-  {
-    idx1 = i * (i-1)/2 + 1
-    idx2 = i * (i+1)/2
-    mat[i, 1:i] = grm[idx1:idx2, 4]
-    mat[1:i, i] = grm[idx1:idx2, 4]
-  }
-  return(mat)
 }
 
 manhattan <- function(dataframe, colors=c("gray10", "gray50"), ymax="max", limitchromosomes=1:23, suggestiveline=-log10(1e-5), genomewideline=NULL, title="", annotate=NULL, ...) {
-  
+
   d=dataframe
   if (!("CHR" %in% names(d) & "BP" %in% names(d) & "P" %in% names(d))) stop("Make sure your data frame contains columns CHR, BP, and P")
   if (any(limitchromosomes)) d=d[d$CHR %in% limitchromosomes, ]
@@ -340,12 +318,12 @@ manhattan <- function(dataframe, colors=c("gray10", "gray50"), ymax="max", limit
   if (!is.null(genomewideline)) {
     abline(h=genomewideline, col="gray")
   } else {
-    abline(h=-log10(0.05/nrow(d)), col="gray")    
+    abline(h=-log10(0.05/nrow(d)), col="gray")
   }
 }
 
 FstPlot <- function(dataframe, colors=c("gray10", "gray50"), ymax="max", limitchromosomes=1:23, suggestiveline=NULL, genomewideline=NULL, annotate=NULL, title="", ...) {
-  
+
   d=dataframe
   if (!("CHR" %in% names(d) & "BP" %in% names(d) & "Fst" %in% names(d))) stop("Make sure your data frame contains columns CHR, BP, and P")
   if (any(limitchromosomes)) d=d[d$CHR %in% limitchromosomes, ]
