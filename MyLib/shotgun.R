206a1b4243bc6d879a5f00802e274aa73c901a62
gc5k <chenguobo@gmail.com>
2018-03-17 22:00:24 +0800

    modified manhattan plot

--- MyLib/shotgun.R
+++ MyLib/shotgun.R
@@ -513,8 +513,9 @@ manhattan <- function(dataframe, colors=c("gray10", "gray50"), ymax="max", limit
   d$logp = -log10(d$P)
   d$pos=NA
   ticks=NULL
-  lastbase=0
-  colors <- rep(colors,max(d$CHR))[1:max(d$CHR)]
+  lastbase=0 #  colors <- rep(colors,max(d$CHR))[1:max(d$CHR)]
+  colors <- rep(colors,max(d$CHR))[1:length(unique(d$CHR))]
+
   if (ymax=="max") ymax<-ceiling(max(d$logp))
   if (ymax<8) ymax<-8
   numchroms=length(unique(d$CHR))
@@ -522,24 +523,26 @@ manhattan <- function(dataframe, colors=c("gray10", "gray50"), ymax="max", limit
     d$pos=d$BP
     ticks=floor(length(d$pos))/2+1
   } else {
-    for (i in unique(d$CHR)) {
+    Uchr=unique(d$CHR)
+    for (i in 1:length(Uchr)) {
       if (i==1) {
-        d[d$CHR==i, ]$pos=d[d$CHR==i, ]$BP
+        d[d$CHR==Uchr[i], ]$pos=d[d$CHR==Uchr[i], ]$BP
       } else {
-        lastbase=lastbase+tail(subset(d,CHR==i-1)$BP, 1)
-        d[d$CHR==i, ]$pos=d[d$CHR==i, ]$BP+lastbase
+        lastbase=lastbase+tail(subset(d, CHR==Uchr[i-1])$BP, 1)
+        d[d$CHR==Uchr[i], ]$pos=d[d$CHR==Uchr[i], ]$BP+lastbase
       }
-      ticks=c(ticks, d[d$CHR==i, ]$pos[floor(length(d[d$CHR==i, ]$pos)/2)+1])
+      ticks=c(ticks, d[d$CHR==Uchr[i], ]$pos[floor(length(d[d$CHR==Uchr[i], ]$pos)/2)+1])
     }
   }
   if (numchroms==1) {
     with(d, plot(main=title, pos, logp, ylim=c(0,ymax), ylab=expression(-log[10](italic(p))), xlab=paste("Chromosome",unique(d$CHR),"position"), ...))
-  }  else {
+  } else {
     with(d, plot(main=title, pos, logp, ylim=c(0,ymax), ylab=expression(-log[10](italic(p))), xlab="Chromosome", xaxt="n", type="n", ...))
     axis(1, at=ticks, lab=unique(d$CHR), ...)
     icol=1
-    for (i in unique(d$CHR)) {
-      with(d[d$CHR==i, ],points(pos, logp, col=colors[icol], ...))
+    Uchr=unique(d$CHR)
+    for (i in 1:length(Uchr)) {
+      with(d[d$CHR==Uchr[i], ], points(pos, logp, col=colors[icol], ...))
       icol=icol+1
     }
   }
