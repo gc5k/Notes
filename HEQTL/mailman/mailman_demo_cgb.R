a12e25540c399efce298af82d7fe37ac95772923
gc5k <chenguobo@gmail.com>
2019-12-29 17:34:25 +0800

    r books

--- HEQTL/mailman/mailman_demo_cgb.R
+++ HEQTL/mailman/mailman_demo_cgb.R
@@ -9,9 +9,9 @@ bs=c(sl^(seq(logSN-1,0)))
 
 A = matrix(sample(sigma, m * n, replace = TRUE), nrow = m, ncol = n)
 
-x1 = c(runif(n, -5, 5))
 N=sl^logSN
-X1=append(x1, rep(0, N-n))
+xDim=10
+x1 = matrix(c(runif(n*xDim, -5, 5), rep(0, xDim*(N-n))), xDim, N, byrow = F)
 
 if (m < n) {
   M=ifelse(m<=logSN, logSN, ceiling(m/logSN)*logSN)
@@ -20,17 +20,18 @@ BLK=ceiling(M/logSN)
 A=rbind(A, matrix(0, nrow=M-m, ncol=n))
 A=cbind(A, matrix(0, nrow=M, ncol=N-n))
 
-#U = buildU(logSN, sigma)
-
+AX=matrix(0, nrow(A), nrow(x1))
 for(Bk in 1:BLK) {
-#  Pcompact=buildPF(A[((Bk-1)*logSN+1):(logSN*Bk),], bs)
   Pcompact=buildPF2(A[((Bk-1)*logSN+1):(logSN*Bk),], bs, n)
 
-  Ax1=mailman_product2(logSN, N, Pcompact, sigma, X1)
-  Ax2= A[((Bk-1)*logSN+1):(logSN*Bk),] %*% X1
-
-  print(paste("Bk", Bk))
-  print(t(Ax1))
-  print(t(Ax2))
-  print(paste("A %*% X1 == Ax1: ", all.equal(Ax2, Ax1)))
+  for(j in 1:xDim) {
+    Ax1=mailman_product2(logSN, N, Pcompact, sigma, x1[j,])
+    AX[((Bk-1)*logSN+1):(logSN*Bk), j] = Ax1
+#    Ax2= A[((Bk-1)*logSN+1):(logSN*Bk),] %*% x1[j,]
+#    print(paste("Bk", Bk))
+#    print(t(Ax1))
+#    print(t(Ax2))
+#    print(paste("A %*% X1 == Ax1: ", all.equal(Ax2, Ax1)))
+  }
 }
+BX=A %*% t(x1)
