d882e35b6b4e43290a179890a2b0ab12bdfb188e
gc5k <chenguobo@gmail.com>
2020-02-20 17:23:49 +0800

    covid

--- HEQTL/mailman/ScalableMoM/K2.R
+++ HEQTL/mailman/ScalableMoM/K2.R
@@ -1,9 +1,9 @@
 source("~/R/MyLib/shotgun.R")
 M=1000
-N=5000
-frq=runif(M, 0.5, 0.5)
+N=500
+frq=runif(M, 0.1, 0.5)
 #frq=runif(M, 0.1, 0.5)
-Dp=sample(c(runif(M/2, 0.9, 1), runif(M/2, -1, -0.9)), M)
+Dp=sample(c(runif(M/2, 0.5, 1), runif(M/2, -1, -0.5)), M)
 
 #simu
 G=GenerateGenoDprime(frq, Dp, N)
@@ -13,13 +13,27 @@ plot(frq, 1-colMeans(G)/2)
 cr=cor(G)
 
 cr2=cr^2
+
 Ecr2=mean(cr2[row(cr2) < col(cr2)])
+Ocr2=mean(cr2[row(cr2) != col(cr2)])
 #standardization
 s=apply(G, 2, scale)
 
 #K
 K=1/M * s %*% t(s)
 K2=K^2
+diagK2=sum(diag(K2))
+mVar=mean(1/(2*FQ*(1-FQ)))
+ss=apply(s^2,1, sum)
+
+me=var(K[row(K)<col(K)])
+R2=(var(K[row(K)<col(K)])-1/M)*M/(M-1)
+EK2=N*(N-1)/M+N+N/M*(mVar-2)+R2*N^2
+SK2=sum(K2)
+
+z=rnorm(N)
+Me=
+r2=(diagK2*M^2/N-sVar-M*(M-1))/(M*(M-1))
 #Moments
 s2=s^2
 
@@ -55,3 +69,8 @@ eK2_diag=
 s4=s^4
 plot(colMeans(s4), 1/(2*frq*(1-frq)))
 abline(a=0, b=1, col="red", lwd=2)
+
+Me <-function(s) {
+  z=matrix(rnorm(nrow(s)), nrow=1, ncol=nrow(s))
+  t2=(z%*%s)^2/nrow(s)
+}
