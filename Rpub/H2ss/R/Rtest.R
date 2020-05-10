8be5de7469b13c08773443e59d17f5d0e8881a0c
gc5k <chenguobo@gmail.com>
2020-05-10 12:05:36 +0800

    h2ss inversion & mit books

--- Rpub/H2ss/R/Rtest.R
+++ Rpub/H2ss/R/Rtest.R
@@ -1,9 +1,6 @@
-n=500 #sample size
+###set up
+n=300 #sample size
 m=500 #marker
-SM=50 #simulation
-BS=25 #randomization factor
-
-LK=0
 
 fq=runif(m, 0.1, 0.5)
 x=matrix(0, n, m)
@@ -14,10 +11,16 @@ sx=apply(x, 2, scale)
 k=sx%*%t(sx)/m
 ksq=k^2
 k2=k%*%t(k)
+k2sq=k2^2
 k4=k2%*%t(k2)
-k4Alt=k2^2
-#evaluate LB
+k4Alt=k2^2 #it is what we are looking for
 
+SM=50 #simulation
+BS=25 #randomization factor
+
+LK=0
+
+#Randominzed algorithm for LB
 LK=array(0, SM)
 for(i in 1:SM) {
   Lb=array(0, BS)
@@ -29,15 +32,14 @@ for(i in 1:SM) {
   }
   LK[i]=sum(Lb)/(BS*m^2)
 }
-print(paste("Obs mean", mean(LK), "vs expected mean", n^2/m+n)) 
-print(paste("obs var", var(LK), " vs expected var", 2*sum(diag(k4))/BS))
-print(paste("incorrected var in Wu's paper", (n^2/m+n)/BS))
-print(paste("sum(diag(K4))=", sum(diag(k4)), "sum[(kkT)^2]", sum(k4Alt)))
-
+print(paste("Obs mean", mean(LK), "vs expected mean", n^2/m+n))
+print(paste("Obs var", var(LK), " vs expected var", 2*sum(diag(k4))/BS))
+print(paste("Incorrect var in Wu's paper:", (n^2/m+n)/BS))
+print(paste("Sum(diag(K4))=", sum(diag(k4)), ", Sum[(kkT)^2]", sum(k4Alt)))
 
 Sum_D_k2_sq=sum(diag(k2))^2
 
-ZX=n^2*m^4+(2*n^3+2*n^2+8*n)*m^3+(n^4+2*n^3+21*n^2+20*n)*m^2+(8*n^3+20*n^2+20*n)*m
+ZX=n^2*m^4+(2*n^3+2*n^2+8*n)*m^3+(n^4+2*n^3+21*n^2+20*n)*m^2+(8*n^3+20*n^2+20*n)*m #zhou's eq 37
 
 print(paste(sum(diag(k2)^2)*n, ZX/m^4, sum(diag(k2))^2))
 
@@ -45,21 +47,13 @@ EU=n*m^2+(n^2+n)*m
 Sum_D_k2=sum(diag(k2))
 print(paste(EU/m^2, Sum_D_k2))
 
-k2_sq=n^2*m^4+(2*n^2+2*n+8)*m^3+(4*n+40)*m^2+48*m
-k2_sq=n^2*m^4+(2*n^2+2*n)*m^3+(4*n)*m^2
+#### basic identity
+Uii=mean(diag(k)) # E(Uii)=1
+Vii=var(diag(k)) #E(Vii)=1/n
+Uij=mean(k[col(k)<row(k)]) #E(Uij)=-1/n
+Vij=var(k[col(k)<row(k)]) #E(Vij)=1/m
 
-sum(diag(k2)^2)
-mean(diag(k2)^2)
-
-ZX/m^4/n
-(n^2*m^4+(2*n^2+6*n-8)*m^3+(24*n+20)*m^2+48*m)/(n*m^4)
-((3*n^2+2*n-4)*m^4+(8*n+4)*m^3+(20*n+24)*m^2+48*m)/(n^2*m^4)
-
-Uii=mean(diag(k))
-Vii=var(diag(k))
-Uij=mean(k[col(k)<row(k)])
-Vij=var(k[col(k)<row(k)])
-
+############# Building GRM identity
 A1=Uii
 A2=Uii^2+Vii
 A3=Uii^3+3*Uii*Vii
@@ -69,17 +63,17 @@ B1=Uij
 B2=Uij^2+Vij
 B3=Uij^3+3*Uij*Vij
 B4=Uij^4+6*Uij^2*Vij+3*Vij^2
+#############
 
-mean(diag(k4))
+#very good
+CGB_pK4D=A4+(n-1)*B4+2*(n-1)*A2*B2+(n-1)*(n-2)*B2*B2
+print(paste(CGB_pK4D, mean(diag(k4Alt), mean(diag(k2sq)))))
 
-pK4D=A4+(n-1)*B4+2*(n-1)*A2*B2+(n-1)*(n-2)*B2*B2
-print(pK4D)
-mean(diag(k4Alt))
-
-pK4O=2*A2*B2+(n-2)*B2*B2+2*A1*B2*A1+(n-2)*A1*B1^3+(n-2)*B1^3*A1+(n-1)*(n-2)*B1^4
-print(pK4O)
+#not that good, we have problems here.
+CGB_pK4O=2*A2*B2
+        +(n-2)*B2*B2
+        +2*A1^2*B2
+        +2*(n-2)*A1*B1^3
+        +(n-1)*(n-2)*B1^4
+print(CGB_pK4O)
 mean(k4Alt[col(k4Alt)<row(k4Alt)])
-
-pK4D*n+n*(n-1)*pK4O
-sum(k4Alt)
-
