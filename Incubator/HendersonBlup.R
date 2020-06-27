1483b8776412d3dbdab2852449bf0247539af751
gc5k <chenguobo@gmail.com>
2020-06-27 17:08:39 +0800

    update r chol

--- Incubator/HendersonBlup.R
+++ Incubator/HendersonBlup.R
@@ -2,11 +2,13 @@ dat=read.table("hendersonMat.txt", as.is = T, header = F)
 dat=as.matrix(dat)
 Lt=chol(dat)
 L=t(Lt)
-
+tm=L
+diag(tm)=1
 D=diag(L[col(L)==row(L)], nrow(L), ncol(L))
 
-Tm=as.matrix(read.table("hendersonT.txt", as.is = T, header = F))
-
-Tmt=t(Tm)
-V=Tm%*%D%*%t(D%*%Tm)
-solve(V)
+#chol factorization
+ch <- chol(dat)
+dd <- diag(ch)
+Lf <- t(ch/dd)
+DD <- diag(dd, nrow(Lf), ncol(Lf))
+Lf%*%DD%*%DD%*%t(Lf)
