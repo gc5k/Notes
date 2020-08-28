5be55191cf9c47dbc5e845911bfa2747f26786c8
gc5k <chenguobo@gmail.com>
2020-08-28 13:02:50 +0800

    blup written up

--- Rpub/zBLUP/blupTest2.R
+++ Rpub/zBLUP/blupTest2.R
@@ -1,29 +1,51 @@
-#walsh example
-y=matrix(c(9, 12, 11, 6, 7, 14), 6, 1)
-x=matrix(0, 6, 2)
-x[,1]=c(1, 0, 1, 1, 1, 0)
-x[,2]=c(0, 1, 0, 0, 0, 1)
-z=matrix(0, 6, 3)
-z[,1]=c(1,1,0,0,0,0)
-z[,2]=c(0,0,1,1,0,0)
-z[,3]=c(0,0,0,0,1,1)
-Se=6
-Sa=2
-V=Sa*z%*%diag(1, 3, 3)%*%t(z)+Se*diag(1, 6, 6)
-VI=solve(V)
+M=200
+N=200
+ha=0.5
+hd=0.3
 
-bL=solve(t(x)%*%VI%*%x)%*%t(x)%*%VI%*%y
-bS=Sa*diag(1, 3, 3)%*%t(z)%*%VI%*%(y-x%*%bL)
+frq=rep(0.5, M)
+G=matrix(rbinom(M*N, 2, frq), N, M)
+Gd=matrix(ifelse(G==1, 1, 0), N, M)
+a=rnorm(M)
+d=rnorm(M)
+BVa=G%*%a
+BVd=Gd%*%d
+
+Beta=matrix(c(1, 2), 2, 1)
+X=matrix(rbinom(2*N, 2, 0.5), N, 2)
+vBVa=var(BVa)[1,1]
+vBVd=var(BVd)[1,1]
+ve=vBVa+vBVd
+y=X%*%Beta+BVa+BVd+rnorm(N, 0, sqrt(ve))
 
 #MME
-R=diag(Se, 6, 6)
-RI=solve(R)
-c11=t(x)%*%x
-c12=t(x)%*%z
-c21=t(c12)
-c22=solve(Sa*diag(1, 3, 3)*Sa/Se)+t(z)%*%z
-MME_m=rbind(cbind(c11, c12), cbind(c21, c22))
-MME_y=rbind(t(x)%*%y, t(z)%*%y)
+C11=t(X)%*%X
+C12=t(X)
+C21=X
+C13=t(X)
+C31=X
+C22=diag(1, M)+1.5*diag(1, M)
+C33=diag(1, M)+3*diag(1, M)
+MME_1=cbind(C11, C12, C13)
+MME_2=cbind(C21, C22, diag(1, M))
+MME_3=cbind(C31, diag(1, M), C33)
 
-MME_b=solve(MME_m)%*%MME_y
-plot(c(bL, bS), MME_b)
+MME_mat=rbind(MME_1, MME_2, MME_3)
+MME_y=matrix(c(t(X)%*%y, y, y), 2*M+2, 1)
+MME_b=solve(MME_mat)%*%MME_y
+plot(BVa, MME_b[3:(M+2),1])
+abline(a=0, b=1)
+plot(BVd, MME_b[(M+3):(nrow(MME_b)),1])
+
+##GLMM
+V=(diag(vBVa, N)+diag(vBVd, N)+diag(ve, N))
+VI=solve(V)
+bEst=solve(t(X)%*%VI%*%X)%*%t(X)%*%VI%*%y
+
+uA=vBVa*VI%*%(y-X%*%bEst)
+uD=vBVd*VI%*%(y-X%*%bEst)
+
+plot(uA, MME_b[3:(M+2),1])
+plot(uD, MME_b[(M+3):(nrow(MME_b)),1])
+
+uD2=(vBVd/vBVa)*uA
